c8f9a7a13c91de2c4461c7335b3ba41081482bc1
Athiyaman Muthukumarasamy <Athi.Muthukumarasamy@medpro.com>
2017-08-17 17:09:08 -0400

    initial version

---
+++ src/main/java/com/paxos/paxosathidemo/PaxosAthiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.paxos.paxosathidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaxosAthiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaxosAthiDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/paxos/paxosathidemo/PaxosAthiDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.paxos.paxosathidemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaxosAthiDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
